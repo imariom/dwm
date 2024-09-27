d7bd9c881142750b7ef66a46490a14dddeef8844
imariom <connect@imariom.com>
2024-09-27 22:12:52 +0200

    project setup

--- config.def.h
+++ config.def.h
@@ -1,25 +1,47 @@
+/* **************************
+ *   |                   | *
+ *   |   i m a r i o m   | *
+ *   |                   | *
+ * 
+ * Mário Moiane (imariom)
+ * http://www.youtube.com/c/imariom
+ * http://www.github.com/imariom/dwm-imariom
+ */
+
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx 	= 10;		/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const int horizpadbar        = 6;		/* horizontal padding for statusbar */
+static const int vertpadbar         = 7;		/* vertical padding for statusbar */
+
+/* Default font will be Inconsolata if installed (sudo pacman -S ttf-inconsolata-nerd ttf-nerd-fonts-symbols).
+ * Otherwise, default font will be Source code (sudo pacman -S ttf-sourcecodepro-nerd)
+ */
+static const char *fonts[] = {
+	"Inconsolata Nerd Font:weight=regular:size=12:antialias=true:hinting=true",
+	"Source Code Pro:size=12:antialias=true:autohint=true"
+};
+
+static const char col_1[] = "#282c34"; /* background color of bar */
+static const char col_2[] = "#282c34"; /* border color unfocused windows */
+static const char col_3[] = "#d7d7d7";
+static const char col_4[] = "#924441"; /* border color focused windows and tags */
+
+static const char *colors[][3] = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_3,     col_1,     col_2 },
+	[SchemeSel]  = { col_3,     col_4,     col_4 },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
+/* static const char *tags[] = { "", "", "", "", """, "", "", "", """}; */
+static const char *tags[] = {"dev", "www", "sys", "chat", "stu", "mus", "vid"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -45,7 +67,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,14 +78,16 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[] 	= { "alacritty", NULL };
+static const char *rofi[] 		= { "rofi", "-show", "drun", "-show-emojis", NULL };
+static const char *vsCode[] 	= { "code", NULL }
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_o, spawn,               {.v = vsCode } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
